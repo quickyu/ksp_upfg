66e5d6ec27de61fe20a14d7958a4853bcf99f605
quick yu <kuai.yu@shnavi.cn>
2025-12-12 17:06:50 +0800

    ``` refactor(ksp_upfg): replace atmospheric control with open loop guidance
    - Replace atmo_control module with flight_manager
    - Use upfg module directly instead of importing all functions
    - Add UPFG target and geo position logging
    - Set mission inclination based on vessel latitude
    - Rename atmospheric_control() to open_loop_guidance()
    ```

--- ksp_upfg.py
+++ ksp_upfg.py
@@ -4,15 +4,20 @@ import numpy as np
 
 import settings
 from utilities import * 
-from atmo_control import *
+from flight_manager import *
 from flight_time_window import *
-from upfg import *
+import upfg
 
 client = krpc.connect(name='Launch to orbit')
 vessel = client.space_center.active_vessel
 print(vessel.name)
 
-setup_upfg_target(vessel)
+upfg_target = upfg.setup_upfg_target(vessel)
+print(f'UPFG target: {upfg_target}')
+
+long, lat = geo_position(vessel)
+print(f'Geo position: {long}, {lat}')
+settings.mission['inclination'] = lat
 
 intercept_time = orbit_intercept_time(vessel, settings.mission['direction'], np.radians(settings.mission['inclination']), np.radians(settings.mission['LAN']))
 liftoff_time = int(client.space_center.ut + intercept_time - settings.mission['launch_time_advance'])
@@ -22,7 +27,7 @@ ft = FlightTimeWindow(liftoff_time)
 
 wait_for_launch(vessel, liftoff_time, True)
 
-atmospheric_control(vessel, upfg_target)
+open_loop_guidance(vessel, upfg_target)
 
 while True:
    time.sleep(1)
