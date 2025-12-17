3fe58198c949a824f450f320177a4889298de846
quick yu <kuai.yu@shnavi.cn>
2025-12-18 07:01:15 +0800

    feat(vessel_state): add ECI and ECEF position/velocity streams
    - add separate streams for ECEF and ECI reference frames
    - include new methods to access position and velocity in both ECEF and ECI coordinates
    - add stream for vessel direction and speed in ECI frame
    - improve code organization by grouping related stream initializations
    - rename existing position/velocity methods to specify ECEF frame
    - add new methods for accessing ECI frame data including direction and speed

--- vessel_state.py
+++ vessel_state.py
@@ -4,17 +4,29 @@ import numpy as np
 
 class VesselState:
    def __init__(self, vessel:Vessel):
-      self.vessel_ = vessel   
-      self.client_ = self.vessel_._client
-      self.space_center_ = self.client_.space_center
+      client = vessel._client
+    
+      self.ut_ = client.add_stream(getattr, client.space_center, 'ut')
 
-      self.ut_ = self.client_.add_stream(getattr, self.client_.space_center, 'ut')
-      self.mass_ = self.client_.add_stream(getattr, vessel, 'mass')
-      self.dry_mass_ = self.client_.add_stream(getattr, vessel, 'dry_mass')
-      self.position_  = self.client_.add_stream(vessel.position, self.vessel_.orbit.body.reference_frame)
-      self.velocity_ = self.client_.add_stream(vessel.velocity, self.vessel_.orbit.body.reference_frame)
-      self.thrust_ = self.client_.add_stream(getattr, vessel, 'thrust') 
-      self.isp_ = self.client_.add_stream(getattr, vessel, 'specific_impulse')
+      self.mass_ = client.add_stream(getattr, vessel, 'mass')
+
+      self.dry_mass_ = client.add_stream(getattr, vessel, 'dry_mass')
+
+      self.position_ecef_  = client.add_stream(vessel.position, vessel.orbit.body.reference_frame)
+
+      self.velocity_ecef_ = client.add_stream(vessel.velocity, vessel.orbit.body.reference_frame)
+
+      self.position_eci_ = client.add_stream(vessel.position, vessel.orbit.body.non_rotating_reference_frame)
+
+      self.velocity_eci_ = client.add_stream(vessel.velocity, vessel.orbit.body.non_rotating_reference_frame)
+
+      self.thrust_ = client.add_stream(getattr, vessel, 'thrust') 
+
+      self.isp_ = client.add_stream(getattr, vessel, 'specific_impulse')
+
+      self.direction_eci_ = client.add_stream(vessel.direction, vessel.orbit.body.non_rotating_reference_frame)
+      
+      self.speed_eci_ = client.add_stream(getattr, vessel.flight(vessel.orbit.body.non_rotating_reference_frame), 'speed')
 
    def universal_time(self) -> float:
       return self.ut_()
@@ -25,14 +37,26 @@ class VesselState:
    def dry_mass(self) -> float:
       return self.dry_mass_()
    
-   def position(self) -> np.ndarray:
-      return np.array(self.position_())
+   def position_ecef(self) -> np.ndarray:
+      return np.array(self.position_ecef_())
    
-   def velocity(self) -> np.ndarray:
-      return np.array(self.velocity_())
+   def velocity_ecef(self) -> np.ndarray:
+      return np.array(self.velocity_ecef_())
+   
+   def position_eci(self) -> np.ndarray:
+      return np.array(self.position_eci_())
+   
+   def velocity_eci(self) -> np.ndarray:
+      return np.array(self.velocity_eci_())
    
    def thrust(self) -> float:
       return self.thrust_()   
    
    def specific_impulse(self) -> float:
       return self.isp_()
+   
+   def direction_eci(self) -> np.ndarray:
+      return np.array(self.direction_eci_())
+   
+   def speed_eci(self) -> float:
+      return self.speed_eci_()
